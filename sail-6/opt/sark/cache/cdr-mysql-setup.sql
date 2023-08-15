1aaa0470ad076eab476f2c3bd93a99be00bf71e7
Jeff Stokoe <jeffstokoe@Jeffs-Mac-mini.local>
2023-08-15 12:29:25 -0400

    First packaging of the PJSIP release

--- sail-6/opt/sark/cache/cdr-mysql-setup.sql
+++ sail-6/opt/sark/cache/cdr-mysql-setup.sql
@@ -8,26 +8,26 @@ GRANT ALL ON asterisk.* TO asterisk@localhost;
 
 USE asterisk; 
 
-CREATE TABLE IF NOT EXISTS cdr ( 
-calldate datetime DEFAULT CURRENT_TIMESTAMP, 
-clid varchar(80) NOT NULL default '', 
-src varchar(80) NOT NULL default '', 
-dst varchar(80) NOT NULL default '', 
-dcontext varchar(80) NOT NULL default '',  
-channel varchar(80) NOT NULL default '', 
-dstchannel varchar(80) NOT NULL default '', 
-lastapp varchar(80) NOT NULL default '', 
-lastdata varchar(80) NOT NULL default '', 
-duration int(11) NOT NULL default '0', 
-billsec int(11) NOT NULL default '0', 
-disposition varchar(45) NOT NULL default '',  
-amaflags int(11) NOT NULL default '0', 
-accountcode varchar(20) NOT NULL default '', 
-userfield varchar(255) NOT NULL default '' 
+CREATE TABLE IF NOT EXISTS `cdr` ( 
+`calldate` datetime NOT NULL DEFAULT (CURRENT_DATE),
+`clid` varchar(80) NOT NULL default '', 
+`src` varchar(80) NOT NULL default '', 
+`dst` varchar(80) NOT NULL default '', 
+`dcontext` varchar(80) NOT NULL default '',  
+`channel` varchar(80) NOT NULL default '', 
+`dstchannel` varchar(80) NOT NULL default '', 
+`lastapp` varchar(80) NOT NULL default '', 
+`lastdata` varchar(80) NOT NULL default '', 
+`duration` int(11) NOT NULL default '0', 
+`billsec` int(11) NOT NULL default '0', 
+`disposition` varchar(45) NOT NULL default '',  
+`amaflags` int(11) NOT NULL default '0', 
+`accountcode` varchar(20) NOT NULL default '', 
+`userfield` varchar(255) NOT NULL default '' 
 ); 
 
-ALTER TABLE cdr ADD INDEX ( calldate ); 
-ALTER TABLE cdr ADD INDEX ( dst ); 
-ALTER TABLE cdr ADD INDEX ( accountcode ); 
+ALTER TABLE `cdr` ADD INDEX ( `calldate` ); 
+ALTER TABLE `cdr` ADD INDEX ( `dst` ); 
+ALTER TABLE `cdr` ADD INDEX ( `accountcode` ); 
 
 FLUSH PRIVILEGES;
